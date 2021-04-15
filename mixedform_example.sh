5aeb42a75938476a7855b50cac57c5f7b3e36614
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-04-15 15:17:36 +0200

    revert mixedfor

--- mixedform_example.sh
+++ mixedform_example.sh
@@ -7,19 +7,3 @@
 	Label4:	4	1	Value4		4	9	18	25	3 \
 	Label5:	5	1	Value5		5	9	18	25	4 \
 	2>out.txt ; cat out.txt ; rm out.txt
-
-./bsddialog --title form --form "Hello World!" 12 30 5 \
-	Label1:	1	1	Value1		1	9	18	25 \
-	Label2:	2	1	Value2		2	9	18	25 \
-	Label3:	3	1	Value3		3	9	18	25 \
-	Label4:	4	1	Value4		4	9	18	25 \
-	Label5:	5	1	Value5		5	9	18	25 \
-	2>out.txt ; cat out.txt ; rm out.txt
-
-./bsddialog --title passwordform --passwordform "Hello World!" 12 30 5 \
-	Label1:	1	1	Value1		1	9	18	25 \
-	Label2:	2	1	Value2		2	9	18	25 \
-	Label3:	3	1	Value3		3	9	18	25 \
-	Label4:	4	1	Value4		4	9	18	25 \
-	Label5:	5	1	Value5		5	9	18	25 \
-	2>out.txt ; cat out.txt ; rm out.txt
