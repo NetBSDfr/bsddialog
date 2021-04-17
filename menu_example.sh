5cdf548934f8506b8a99d2a6c0064dc79df81662
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-04-17 19:16:37 +0200

    use bsddialog

--- menu_example.sh
+++ menu_example.sh
@@ -7,3 +7,11 @@
 	"Tag 4"	"DESC 4 xyz" \
 	"Tag 5"	"DESC 5 xyz" \
 	2>out.txt ; cat out.txt ; rm out.txt
+
+dialog --title menu --menu "Hello World!" 18 30 8 \
+	"Tag 1"	"DESC 1 xyz" \
+	"Tag 2"	"DESC 2 xyz" \
+	"Tag 3"	"DESC 3 xyz" \
+	"Tag 4"	"DESC 4 xyz" \
+	"Tag 5"	"DESC 5 xyz" \
+	2>out.txt ; cat out.txt ; rm out.txt
