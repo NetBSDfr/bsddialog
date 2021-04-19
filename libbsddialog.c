6c6a249f15428f821148b0f9c288b21a275854d2
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-04-19 03:29:09 +0200

    patch gauge render

--- libbsddialog.c
+++ libbsddialog.c
@@ -1302,9 +1302,19 @@ int bsddialog_gauge(struct config conf, char* text, int rows, int cols, int perc
 
 	y = conf.y;
 	x = conf.x;
-	widget = shadow = NULL;
+/*	widget = shadow = NULL;
 	if (widget_init(conf, widget, &y, &x, text, &rows, &cols, shadow) < 0)
-		return -1;
+		return -1;*/
+
+	if (conf.shadow) {
+		shadow = newwin(rows, cols+1, conf.y+1, conf.x+1);
+		wbkgd(shadow, COLOR_PAIR(BLACK_BLACK));
+		wrefresh(shadow);
+	}
+
+	widget = new_window(y, x, rows, cols, conf.title, NULL,
+	    conf.no_lines ? NOLINES : RAISED, conf.ascii_lines, false);
+	print_text_multiline(widget, 1, 2, text, cols - 4);
 
 	bar = new_window(y+rows -4, x+3, 3, cols-6, NULL, conf.hline,
 	    conf.no_lines ? NOLINES : RAISED, conf.ascii_lines, false);
