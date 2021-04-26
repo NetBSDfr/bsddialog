393c77217a6b559dd58c3537367d9b9a7654f828
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-04-27 01:13:27 +0200

    add example

---
+++ library_examples/treeview.c
@@ -0,0 +1,50 @@
+/*-
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Written in 2021 by Alfonso Sabato Siciliano.
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty, see:
+ *   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../bsddialog.h"
+
+/* cc ../theme.c ../libbsddialog.c treeview.c -o treeview -lform -lncurses */
+int main()
+{
+	int i, output;
+	struct config conf; //bsddialog_config conf;
+	struct bsddialog_menuitem items[5] = {
+	    {false, 0, "Name 1", "Desc 1", "Bottom Desc 1"},
+	    {false, 0, "Name 2", "Desc 2", "Bottom Desc 2"},
+	    {false, 0, "Name 3", "Desc 3", "Bottom Desc 3"},
+	    {false, 0, "Name 4", "Desc 4", "Bottom Desc 4"},
+	    {false, 0, "Name 5", "Desc 5", "Bottom Desc 5"}
+	};
+
+	memset(&conf, 0, sizeof(struct config));
+	conf.y = conf.x = -1;
+	conf.shadow = true;
+	conf.item_help = true;
+	conf.cancel_label = "Quit";
+	conf.ok_label = "Submit";
+	conf.title = "radiolist";
+	
+	if (bsddialog_init() < 0)
+		return -1;
+
+	output = bsddialog_treeview(conf, "Example", 15, 30, 5, 5, items);
+
+	bsddialog_end();
+
+	printf("Treeview:\n");
+	for (i=0; i<5; i++)
+		printf(" (%c) %s\n", items[i].on ? '*' : ' ', items[i].name);
+		
+	
+	return output;
+}
