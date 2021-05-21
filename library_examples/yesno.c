9ea9b6e1ab08e00ba9ed2ec4c2e9f7cdc211d8fe
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-05-21 11:33:43 +0200

    add new examples

---
+++ library_examples/yesno.c
@@ -0,0 +1,34 @@
+/*-
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Written in 2021 by Alfonso Sabato Siciliano.
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty, see:
+ *   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../bsddialog.h"
+
+int main()
+{
+	int output;
+	struct config conf; //bsddialog_config conf;
+
+	memset(&conf, 0, sizeof(struct config));
+	conf.y = conf.x = -1;
+	conf.shadow = true;
+	conf.title = "yesno";
+	
+	if (bsddialog_init() < 0)
+		return -1;
+
+	output = bsddialog_yesno(conf, "Example", 7, 25);
+
+	bsddialog_end();
+
+	return output;
+}
