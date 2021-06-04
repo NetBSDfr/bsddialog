886cdc7aa34de5abc859bb8b002e144cab0beb48
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-06-04 18:04:23 +0200

    add debug macro

--- lib_util.h
+++ lib_util.h
@@ -32,6 +32,11 @@
  * Utils to implement widgets
  */
 
+#define BSDDIALOG_DEBUG(y,x,fmt, ...) do {			\
+	mvprintw(y, x, fmt, __VA_ARGS__);			\
+	refresh();						\
+} while (0)
+
 #define	MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
