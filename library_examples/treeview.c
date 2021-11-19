5a93f9619f21e3a8b98e5aaf90114156f639507f
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-11-19 19:25:53 +0100

    update treeview example

--- library_examples/treeview.c
+++ library_examples/treeview.c
@@ -19,10 +19,10 @@ int main()
 	struct bsddialog_conf conf;
 	struct bsddialog_menuitem items[5] = {
 	    {"", false, 0, "Name 1", "Desc 1", "Bottom Desc 1"},
-	    {"", false, 0, "Name 2", "Desc 2", "Bottom Desc 2"},
-	    {"", false, 0, "Name 3", "Desc 3", "Bottom Desc 3"},
-	    {"", false, 0, "Name 4", "Desc 4", "Bottom Desc 4"},
-	    {"", false, 0, "Name 5", "Desc 5", "Bottom Desc 5"}
+	    {"", false, 1, "Name 2", "Desc 2", "Bottom Desc 2"},
+	    {"", false, 1, "Name 3", "Desc 3", "Bottom Desc 3"},
+	    {"", false, 2, "Name 4", "Desc 4", "Bottom Desc 4"},
+	    {"", false, 1, "Name 5", "Desc 5", "Bottom Desc 5"}
 	};
 
 	memset(&conf, 0, sizeof(struct bsddialog_conf));
