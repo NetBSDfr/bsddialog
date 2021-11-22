d1b63641549b01af82a0af84e29f35a58f942766
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-11-22 03:03:58 +0100

    add blackwhite theme to example

--- library_examples/theme.c
+++ library_examples/theme.c
@@ -19,25 +19,26 @@ int main()
 	int output;
 	struct bsddialog_conf conf;
 	enum bsddialog_default_theme theme;
-	struct bsddialog_menuitem items[4] = {
-	    {"", false, 0, "Dialog",   "Current dialog theme", "BSDDIALOG_THEME_DIALOG" },
-	    {"", false, 0, "BSDDialog","Future default theme", "BSDDIALOG_THEME_DEFAULT"},
-	    {"", false, 0, "Magenta",  "Testing",              "BSDDIALOG_THEME_MAGENTA"},
-	    {"", false, 0, "Quit",     "Exit",                 "Quit or Cancel to exit" }
+	struct bsddialog_menuitem items[5] = {
+	    {"", false, 0, "Dialog",    "Current dialog theme",  "BSDDIALOG_THEME_DIALOG" },
+	    {"", false, 0, "BSDDialog", "Future default theme",  "BSDDIALOG_THEME_DEFAULT"},
+	    {"", false, 0, "BlackWhite","Black and White theme", "BSDDIALOG_THEME_BLACKWHITE"},
+	    {"", false, 0, "Magenta",   "Testing",               "BSDDIALOG_THEME_MAGENTA"},
+	    {"", false, 0, "Quit",      "Exit",                  "Quit or Cancel to exit" }
 	};
 
 	bsddialog_initconf(&conf);
 	conf.title = " Theme ";
 	
-	if (bsddialog_init() < 0)
-		return -1;
+	if (bsddialog_init() == BSDDIALOG_ERROR)
+		return BSDDIALOG_ERROR;
 
 	while (true) {
 		bsddialog_backtitle(conf, "Theme Example");
 
-		output = bsddialog_menu(conf, "Choose theme", 15, 40, 4, 4, items, NULL);
+		output = bsddialog_menu(conf, "Choose theme", 15, 40, 5, 5, items, NULL);
 
-		if (output != BSDDIALOG_YESOK || items[3].on)
+		if (output != BSDDIALOG_YESOK || items[4].on)
 			break;
 
 		if (items[0].on) {
@@ -49,8 +50,12 @@ int main()
 			conf.menu.default_item = items[1].name;
 		}
 		else if (items[2].on) {
-			theme = BSDDIALOG_THEME_MAGENTA;
+			theme = BSDDIALOG_THEME_BLACKWHITE;
 			conf.menu.default_item = items[2].name;
+		}
+		else if (items[3].on) {
+			theme = BSDDIALOG_THEME_MAGENTA;
+			conf.menu.default_item = items[3].name;
 		}
 
 		bsddialog_set_default_theme(theme);
