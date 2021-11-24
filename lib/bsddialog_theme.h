17e097a94e16205b1d589a2e5bd3a6fd2322e866
Baptiste Daroussin <baptiste.daroussin@qorvo.com>
2021-11-24 15:49:16 +0100

    Fix prototype

--- lib/bsddialog_theme.h
+++ lib/bsddialog_theme.h
@@ -89,7 +89,7 @@ enum bsddialog_default_theme {
 };
 
 int bsddialog_color(enum bsddialog_color background, enum bsddialog_color foreground);
-struct bsddialog_theme bsddialog_get_theme();
+struct bsddialog_theme bsddialog_get_theme(void);
 void bsddialog_set_theme(struct bsddialog_theme theme);
 int bsddialog_set_default_theme(enum bsddialog_default_theme theme);
 
