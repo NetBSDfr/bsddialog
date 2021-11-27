b61bfc5c87d3bf15cf31e6b8b349536b8cc66fd0
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-11-27 01:38:01 +0100

    update example for current features

--- examples_library/form.c
+++ examples_library/form.c
@@ -21,11 +21,10 @@ int main()
 	int i, output;
 	struct bsddialog_conf conf;
 	char *v1, *v2, *v3, *v4, *v5, *v6, *v7, *v8;
-	struct bsddialog_formitem items[4] = {
-		{"Label 1:", 1, 1, "init 1", 1, 10, 50, 20, v1, v2, 0},
-		{"Label 2:", 2, 1, "init 2", 2, 10, 50, 20, v3, v4, RO},
-		{"Label 3:", 3, 1, "init 3", 3, 10, 50, 20, v5, v6, HIDDEN},
-		{"Label 4:", 4, 1, "init 4", 4, 10, 50, 20, v7, v8, HIDDEN | RO}
+	struct bsddialog_formitem items[3] = {
+		{"Input:",    1, 1, "value",     1, 11, 50, 20, v1, v2, 0},
+		{"Input:",    2, 1, "read only", 2, 11, 50, 20, v3, v4, RO},
+		{"Password:", 3, 1, "",          3, 11, 50, 20, v5, v6, HIDDEN}
 	};
 
 	bsddialog_initconf(&conf);
@@ -34,7 +33,7 @@ int main()
 	if (bsddialog_init() < 0)
 		return -1;
 
-	output = bsddialog_form(conf, "Forms", 20, 50, 4, 4, items);
+	output = bsddialog_form(conf, "Forms", 20, 50, 3, 3, items);
 
 	bsddialog_end();
 	
@@ -42,7 +41,7 @@ int main()
 		printf("Error: %s", bsddialog_geterror());
 
 	printf("Values:\n");
-	for (i=0; i<4; i++)
+	for (i=0; i<3; i++)
 		printf("%s|-|%s|-|%s|\n", items[i].label, items[i].newvalue1, items[i].newvalue2);
 	
 	return output;
