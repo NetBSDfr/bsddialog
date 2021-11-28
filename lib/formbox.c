78db71b435f887b7baaa7ff9d313176ce40c1d20
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-11-28 02:59:41 +0100

    comment

--- lib/formbox.c
+++ lib/formbox.c
@@ -217,6 +217,10 @@ form_handler(WINDOW *widget, int y, int cols, struct buttons bs, WINDOW *formwin
 				shiftleft(mf);
 			break;
 		default:
+			/*
+			 * user input, add unicode chars to "public" buffer
+			 */
+			
 			if (informwin) {
 				mf = GETMYFIELD2(form);
 				if (mf->secure)
