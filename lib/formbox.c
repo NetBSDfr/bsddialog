059ad73bbca03d2662654e509038a70380d86cdc
Alfonso Siciliano <alfsiciliano@gmail.com>
2021-12-15 05:48:33 +0100

    fix forms

--- lib/formbox.c
+++ lib/formbox.c
@@ -51,10 +51,10 @@ extern struct bsddialog_theme t;
 
 /* util struct for private buffer and view options */
 struct myfield {
-	int len;
+	int buflen;
 	char *buf;
 	int pos;
-	int size;
+	int maxpos;
 	bool secure;
 	int securech;
 	char *bottomdesc;
@@ -66,30 +66,32 @@ static void insertch(struct myfield *mf, int ch)
 {
 	int i;
 
-	if (mf->len == mf->size)
+	if (mf->buflen > mf->maxpos)
 		return;
 
-	for (i=mf->len-1; i>=mf->pos; i--) {
+	for (i = mf->buflen; i >= mf->pos; i--) {
 		mf->buf[i+1] = mf->buf[i];
 	}
 
 	mf->buf[mf->pos] = ch;
 	mf->pos += 1;
-	mf->len += 1;
-	mf->buf[mf->len] = '\0';
+	if (mf->pos > mf->maxpos)
+		mf->pos = mf->maxpos;
+	mf->buflen += 1;
+	mf->buf[mf->buflen] = '\0';
 }
 
 static void shiftleft(struct myfield *mf)
 {
 	int i, last;
 
-	for (i=mf->pos; i<mf->len; i++) {
+	for (i = mf->pos; i < mf->buflen -1; i++) {
 		mf->buf[i] = mf->buf[i+1];
 	}
 
-	last = mf->len > 0 ? mf->len -1 : 0;
+	last = mf->buflen > 0 ? mf->buflen -1 : 0;
 	mf->buf[last] = '\0';
-		mf->len = last;
+		mf->buflen = last;
 }
 
 static void print_bottomdesc(struct myfield *mf)
@@ -120,7 +122,7 @@ form_handler(struct bsddialog_conf *conf, WINDOW *widget, int y, int cols,
 	form_driver(form, REQ_END_LINE);
 	mf = GETMYFIELD2(form);
 	print_bottomdesc(mf);
-	mf->pos = mf->len;
+	mf->pos = MIN(mf->buflen, mf->maxpos);
 	while(loop) {
 		if (buttupdate) {
 			draw_buttons(widget, y, cols, bs, !informwin);
@@ -196,7 +198,7 @@ form_handler(struct bsddialog_conf *conf, WINDOW *widget, int y, int cols,
 		case KEY_RIGHT:
 			if (informwin) {
 				mf = GETMYFIELD2(form);
-				if (mf->pos >= mf->len)
+				if (mf->pos >= mf->buflen)
 					break;
 				mf->pos += 1;
 				form_driver(form, REQ_NEXT_CHAR);
@@ -216,7 +218,7 @@ form_handler(struct bsddialog_conf *conf, WINDOW *widget, int y, int cols,
 			form_driver(form, REQ_END_LINE);
 			mf = GETMYFIELD2(form);
 			print_bottomdesc(mf);
-			mf->pos = mf->len;
+			mf->pos = MIN(mf->buflen, mf->maxpos);
 			set_field_fore(current_field(form), t.form.f_fieldcolor);
 			set_field_back(current_field(form), t.form.f_fieldcolor);
 			break;
@@ -229,7 +231,7 @@ form_handler(struct bsddialog_conf *conf, WINDOW *widget, int y, int cols,
 			form_driver(form, REQ_END_LINE);
 			mf = GETMYFIELD2(form);
 			print_bottomdesc(mf);
-			mf->pos = mf->len;
+			mf->pos = MIN(mf->buflen, mf->maxpos);
 			set_field_fore(current_field(form), t.form.f_fieldcolor);
 			set_field_back(current_field(form), t.form.f_fieldcolor);
 			break;
@@ -239,14 +241,13 @@ form_handler(struct bsddialog_conf *conf, WINDOW *widget, int y, int cols,
 			if (mf->pos <= 0)
 				break;
 			form_driver(form, REQ_DEL_PREV);
-			mf = GETMYFIELD2(form);
-			mf->pos -= 1;
+			mf->pos = mf->pos - 1;
 			shiftleft(mf);
 			break;
 		case KEY_DC:
 			form_driver(form, REQ_DEL_CHAR);
 			mf = GETMYFIELD2(form);
-			if (mf->len-1 >= mf->pos)
+			if (mf->pos < mf->buflen)
 				shiftleft(mf);
 			break;
 		case KEY_F(1):
@@ -386,6 +387,15 @@ bsddialog_form(struct bsddialog_conf *conf, char* text, int rows, int cols,
 	if (formheight < nitems)
 		formheight = nitems;
 
+	for (i = 0; i < nitems; i++) {
+		if (items[i].maxvaluelen == 0)
+			RETURN_ERROR("maxvaluelen cannot be zero");
+		if (items[i].fieldlen == 0)
+			RETURN_ERROR("fieldlen cannot be zero");
+		if (items[i].fieldlen > items[i].maxvaluelen)
+			RETURN_ERROR("fieldlen cannot be > maxvaluelen");
+	}
+
 	maxline = 0;
 	myfields = malloc(nitems * sizeof(struct myfield));
 	cfield = calloc(nitems + 1, sizeof(FIELD*));
@@ -396,12 +406,15 @@ bsddialog_form(struct bsddialog_conf *conf, char* text, int rows, int cols,
 		set_max_field(cfield[i], items[i].maxvaluelen);
 		set_field_buffer(cfield[i], 0, items[i].init);
 
-		myfields[i].pos  = strlen(items[i].init);
-		myfields[i].len  = strlen(items[i].init);
-		myfields[i].size = items[i].maxvaluelen;
-		myfields[i].buf  = malloc(myfields[i].size);
-		memset(myfields[i].buf, 0, myfields[i].size);
-		strcpy(myfields[i].buf, items[i].init);
+		myfields[i].buf = malloc(items[i].maxvaluelen + 1);
+		memset(myfields[i].buf, 0, items[i].maxvaluelen + 1); // end with '\0' for strdup
+		strncpy(myfields[i].buf, items[i].init, items[i].maxvaluelen);
+
+		myfields[i].buflen = strlen(myfields[i].buf);
+
+		myfields[i].maxpos = items[i].maxvaluelen -1;
+		myfields[i].pos = MIN(myfields[i].buflen, myfields[i].maxpos);
+
 		myfields[i].bottomdesc = items[i].bottomdesc;
 		set_field_userptr(cfield[i], &myfields[i]);
 
