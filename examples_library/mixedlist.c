09590fa1886e81582451578558a2dfa46ebdde95
Alfonso Siciliano <alfsiciliano@gmail.com>
2022-02-10 02:18:57 +0100

    update example

--- examples_library/mixedlist.c
+++ examples_library/mixedlist.c
@@ -29,8 +29,9 @@ int main()
 	    { "" , false, 0, "Name 4", "Desc 4", "Bottom Desc 4" },
 	    { "+", true,  0, "Name 5", "Desc 5", "Bottom Desc 5" }
 	};
-	struct bsddialog_menuitem sep2[1] = {
-	    { "", true, 0, "Radiolist", "(desc)", "" }
+	struct bsddialog_menuitem sep2[2] = {
+	    { "", true, 0, "Radiolist", "(desc)", "" },
+	    { "", true, 0, "(Sub", "desc)",       "" }
 	};
 	struct bsddialog_menuitem radio[5] = {
 	    { "",  true,  0, "Name 1", "Desc 1", "Bottom Desc 1" },
@@ -42,7 +43,7 @@ int main()
 	struct bsddialog_menugroup group[4] = {
 	    { BSDDIALOG_SEPARATOR, 1, sep1  },
 	    { BSDDIALOG_CHECKLIST, 5, check },
-	    { BSDDIALOG_SEPARATOR, 1, sep2  },
+	    { BSDDIALOG_SEPARATOR, 2, sep2  },
 	    { BSDDIALOG_RADIOLIST, 5, radio }
 	};
 
@@ -53,7 +54,7 @@ int main()
 
 	bsddialog_initconf(&conf);
 	conf.title = "mixedlist";
-	output = bsddialog_mixedlist(&conf, "Example", 20, 0, 12, 4, group,
+	output = bsddialog_mixedlist(&conf, "Example", 20, 0, 13, 4, group,
 	    NULL, NULL);
 
 	bsddialog_end();
