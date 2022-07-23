d8d4005d4fbe792d4f8a141fc1673ac3793e0616
Alfonso Siciliano <alfsiciliano@gmail.com>
2022-07-23 15:49:34 +0200

    add examples_library/margin.c

---
+++ examples_library/margin.c
@@ -0,0 +1,44 @@
+/*-
+ * SPDX-License-Identifier: CC0-1.0
+ *
+ * Written in 2022 by Alfonso Sabato Siciliano.
+ * To the extent possible under law, the author has dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty, see:
+ *   <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <bsddialog.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int output;
+	struct bsddialog_conf conf;
+
+	if (bsddialog_init() == BSDDIALOG_ERROR) {
+		printf("Error: %s\n", bsddialog_geterror());
+		return (1);
+	}
+
+	bsddialog_initconf(&conf);
+	conf.title = "Margins";
+	conf.y = 15;
+	conf.auto_topmargin = 15;
+	conf.auto_downmargin = 2;
+	output = bsddialog_msgbox(&conf, "Example", 0, 0);
+
+	bsddialog_end();
+
+	switch (output) {
+	case BSDDIALOG_ERROR:
+		printf("Error %s\n", bsddialog_geterror());
+		break;
+	case BSDDIALOG_OK:
+		printf("OK\n");
+		break;
+	}
+
+	return (output);
+}
