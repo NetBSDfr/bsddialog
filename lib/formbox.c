8b14f5813b17cf3c4cebb32734ff941f0f4e2093
Alfonso Siciliano <alfsiciliano@gmail.com>
2022-08-11 02:24:08 +0200

    free internal form buffers

--- lib/formbox.c
+++ lib/formbox.c
@@ -659,8 +659,10 @@ bsddialog_form(struct bsddialog_conf *conf, const char *text, int rows,
 			if (conf->key.f1_file == NULL &&
 			    conf->key.f1_message == NULL)
 				break;
-			if (f1help(conf) != 0)
-				return (BSDDIALOG_ERROR);
+			if (f1help(conf) != 0) {
+				output = BSDDIALOG_ERROR;
+				loop = false;
+			}
 			/* No Break */
 		case KEY_RESIZE:
 			if (update_dialog(conf, shadow, widget, y, x, h, w,
@@ -712,6 +714,10 @@ bsddialog_form(struct bsddialog_conf *conf, const char *text, int rows,
 	curs_set(0);
 
 	delwin(formwin);
+	for (i = 0; i < nitems; i++) {
+		free(items[i].privwbuf);
+		free(items[i].pubwbuf);
+	}
 	end_dialog(conf, shadow, widget, textpad);
 
 	return (output);
