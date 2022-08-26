ccc3bac18c1bc0719b3dfafb81fa0d638407bba3
Alfonso Siciliano <alfix86@gmail.com>
2022-08-26 17:49:11 +0000

    Revert "tmp test cursor 2"
    This reverts commit ed309fbc0c3f359186d6f4b58a7c8122b39bd812

--- lib/formbox.c
+++ lib/formbox.c
@@ -240,7 +240,7 @@ drawitem(struct privateform *form, struct privateitem *item, bool focus)
 		refresh();
 	}
 
-	curs_set((focus && item->cursor) ? 2 : 0 );
+	curs_set((focus && item->cursor) ? 1 : 0 );
 	wmove(form->pad, item->yfield, item->xfield + item->xcursor);
 	prefresh(form->pad, form->y, 0, form->ys, form->xs, form->ye, form->xe);
 }
