bfdccac19d596751474bbd8e7884e58d96120f1c
Alfonso Siciliano <alfix86@gmil.com>
2023-07-04 23:51:49 +0200

    refactor messagebox.c

--- lib/messagebox.c
+++ lib/messagebox.c
@@ -33,18 +33,19 @@
 #include "lib_util.h"
 
 static void
-textupdate(WINDOW *widget, WINDOW *textpad, int htextpad, int ytextpad,
-    bool hastext)
+textupdate(WINDOW *widget, WINDOW *textpad, int ytextpad, int htextpad,
+    bool htext)
 {
-	int y, x, h, w;
+	int printrows, y, x, h, w;
 
 	getbegyx(widget, y, x);
 	getmaxyx(widget, h, w);
+	printrows = h - BORDER - HBUTTONS - BORDER;
 
-	if (hastext && htextpad > h - 4) {
+	if (htext > 0 && htextpad > printrows) {
 		wattron(widget, t.dialog.arrowcolor);
-		mvwprintw(widget, h-3, w-6, "%3d%%",
-		    100 * (ytextpad+h-4)/ htextpad);
+		mvwprintw(widget, h-HBUTTONS-BORDER, w-4-TEXTHMARGIN-BORDER,
+		    "%3d%%", 100 * (ytextpad + printrows) / htextpad);
 		wattroff(widget, t.dialog.arrowcolor);
 		wnoutrefresh(widget);
 	}
@@ -64,7 +65,7 @@ message_size_position(struct bsddialog_conf *conf, int rows, int cols,
 	if (set_widget_autosize(conf, rows, cols, h, w, text,
 	    (*htext < 0) ? htext : NULL, bs, 0, 0) != 0)
 		return (BSDDIALOG_ERROR);
-	minw = (*htext > 0) ? 1 + TEXTHMARGINS: 0;
+	minw = (*htext > 0) ? 1 + TEXTHMARGINS: 0 ;
 	if (widget_checksize(*h, *w, bs, MIN(*htext, 1), minw) != 0)
 		return (BSDDIALOG_ERROR);
 	if (set_widget_position(conf, y, x, *h, *w) != 0)
@@ -77,7 +78,7 @@ static int
 do_message(struct bsddialog_conf *conf, const char *text, int rows, int cols,
     struct buttons bs)
 {
-	bool hastext, loop;
+	bool loop;
 	int y, x, h, w, retval, htext, ytextpad, htextpad, printrows, unused;
 	WINDOW *widget, *textpad, *shadow;
 	wint_t input;
@@ -86,17 +87,16 @@ do_message(struct bsddialog_conf *conf, const char *text, int rows, int cols,
 	if (message_size_position(conf, rows, cols, text, &bs, &y, &x, &h, &w,
 	    &htext) != 0)
 		return (BSDDIALOG_ERROR);
-	hastext = (htext > 0) ? true : false;
 	if (new_dialog(conf, &shadow, &widget, y, x, h, w, &textpad, text, &bs) != 0)
 		return (BSDDIALOG_ERROR);
 
-	printrows = h - 4;
+	printrows = h - BORDER - HBUTTONS - BORDER;
 	ytextpad = 0;
 	getmaxyx(textpad, htextpad, unused);
 	unused++; /* fix unused error */
 	loop = true;
 	while (loop) {
-		textupdate(widget, textpad, htextpad, ytextpad, hastext);
+		textupdate(widget, textpad, ytextpad, htextpad, htext);
 		doupdate();
 		if (get_wch(&input) == ERR)
 			continue;
@@ -174,10 +174,10 @@ do_message(struct bsddialog_conf *conf, const char *text, int rows, int cols,
 			    textpad, text, &bs) != 0)
 				return (BSDDIALOG_ERROR);
 
-			printrows = h - 4;
+			printrows = h - BORDER - HBUTTONS - BORDER;
 			getmaxyx(textpad, htextpad, unused);
 			ytextpad = 0;
-			textupdate(widget, textpad, htextpad, ytextpad, hastext);
+			textupdate(widget, textpad, ytextpad, htextpad, htext);
 
 			/* Important to fix grey lines expanding screen */
 			refresh();
