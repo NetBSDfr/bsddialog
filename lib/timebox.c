194fcf2b0526104fa190e3085f5c2116f5b15ab0
Alfonso Siciliano <alfix86@gmil.com>
2023-07-11 22:53:07 +0200

    convert timebox to use struct dialog

--- lib/timebox.c
+++ lib/timebox.c
@@ -34,6 +34,8 @@
 #include "lib_util.h"
 
 #define MINWTIME   14 /* 3 windows and their borders */
+#define HBOX        3
+#define WBOX        4
 
 struct clock {
 	unsigned int max;
@@ -45,10 +47,7 @@ static void
 drawsquare(struct bsddialog_conf *conf, WINDOW *win, unsigned int value,
     bool focus)
 {
-	int h, w;
-
-	getmaxyx(win, h, w);
-	draw_borders(conf, win, h, w, LOWERED);
+	draw_borders(conf, win, LOWERED);
 	if (focus) {
 		wattron(win, t.dialog.arrowcolor);
 		mvwhline(win, 0, 1, conf->ascii_lines ? '^' : ACS_UARROW, 2);
@@ -62,7 +61,35 @@ drawsquare(struct bsddialog_conf *conf, WINDOW *win, unsigned int value,
 	if (focus)
 		wattroff(win, t.menu.f_namecolor);
 
-	wrefresh(win);
+	wnoutrefresh(win);
+}
+
+static int timebox_redraw(struct dialog *d, struct clock *c)
+{
+	int y, x;
+
+	if (d->built) {
+		hide_dialog(d);
+		refresh(); /* Important for decreasing screen */
+	}
+	if (dialog_size_position(d, HBOX, MINWTIME, NULL) != 0)
+		return (BSDDIALOG_ERROR);
+	if (draw_dialog(d) != 0)
+		return (BSDDIALOG_ERROR);
+	if (d->built)
+		refresh(); /* Important to fix grey lines expanding screen */
+	TEXTPAD(d, HBOX + HBUTTONS);
+
+	y = d->y + d->h - BORDER - HBUTTONS - HBOX;
+	x = d->x + d->w/2 - 7;
+	update_box(d->conf, c[0].win, y, x, HBOX, WBOX, LOWERED);
+	mvwaddch(d->widget, d->h - 5, d->w/2 - 3, ':');
+	update_box(d->conf, c[1].win, y, x += 5, HBOX, WBOX, LOWERED);
+	mvwaddch(d->widget, d->h - 5, d->w/2 + 2, ':');
+	update_box(d->conf, c[2].win, y, x + 5, HBOX, WBOX, LOWERED);
+	wnoutrefresh(d->widget); /* for mvwaddch(':') */
+
+	return (0);
 }
 
 int
@@ -70,10 +97,9 @@ bsddialog_timebox(struct bsddialog_conf *conf, const char* text, int rows,
     int cols, unsigned int *hh, unsigned int *mm, unsigned int *ss)
 {
 	bool loop, focusbuttons;
-	int i, retval, y, x, h, w, sel;
+	int i, retval, sel;
 	wint_t input;
-	WINDOW *widget, *textpad, *shadow;
-	struct buttons bs;
+	struct dialog d;
 	struct clock c[3] = {
 		{23, *hh, NULL},
 		{59, *mm, NULL},
@@ -84,42 +110,31 @@ bsddialog_timebox(struct bsddialog_conf *conf, const char* text, int rows,
 	CHECK_PTR(mm, unsigned int);
 	CHECK_PTR(ss, unsigned int);
 
-	for (i = 0 ; i < 3; i++) {
-		if (c[i].value > c[i].max)
-			c[i].value = c[i].max;
+	if (prepare_dialog(conf, text, rows, cols, &d) != 0)
+		return (BSDDIALOG_ERROR);
+	set_buttons(&d, true, BUTTON_OK_LABEL, BUTTON_CANCEL_LABEL);
+	for (i=0; i<3; i++) {
+		if ((c[i].win = newwin(1, 1, 1, 1)) == NULL)
+			RETURN_FMTERROR("Cannot build WINDOW for time[%d]", i);
+		wbkgd(c[i].win, t.dialog.color);
+		c[i].value = MIN(c[i].value, c[i].max);
 	}
-
-	get_buttons(conf, &bs, true, BUTTON_OK_LABEL, BUTTON_CANCEL_LABEL);
-	if (widget_size_position(conf, rows, cols, text, 3 /*windows*/,
-	    MINWTIME, &bs, &y, &x, &h, &w, NULL) != 0)
+	if (timebox_redraw(&d, c) != 0)
 		return (BSDDIALOG_ERROR);
-	if (new_dialog(conf, &shadow, &widget, y, x, h, w, &textpad, text, &bs) != 0)
-		return (BSDDIALOG_ERROR);
-
-	pnoutrefresh(textpad, 0, 0, y+1, x+2, y+h-7, x+w-2);
-	doupdate();
-
-	c[0].win = new_boxed_window(conf, y+h-6, x + w/2 - 7, 3, 4, LOWERED);
-	mvwaddch(widget, h - 5, w/2 - 3, ':');
-	c[1].win = new_boxed_window(conf, y+h-6, x + w/2 - 2, 3, 4, LOWERED);
-	mvwaddch(widget, h - 5, w/2 + 2, ':');
-	c[2].win = new_boxed_window(conf, y+h-6, x + w/2 + 3, 3, 4, LOWERED);
-
-	wrefresh(widget);
 
 	sel = -1;
 	loop = focusbuttons = true;
 	while (loop) {
 		for (i = 0; i < 3; i++)
 			drawsquare(conf, c[i].win, c[i].value, sel == i);
-
+		doupdate();
 		if (get_wch(&input) == ERR)
 			continue;
 		switch(input) {
 		case KEY_ENTER:
 		case 10: /* Enter */
 			if (focusbuttons || conf->button.always_active) {
-				retval = BUTTONVALUE(bs);
+				retval = BUTTONVALUE(d.bs);
 				loop = false;
 			}
 			break;
@@ -132,49 +147,47 @@ bsddialog_timebox(struct bsddialog_conf *conf, const char* text, int rows,
 		case KEY_RIGHT:
 		case '\t': /* TAB */
 			if (focusbuttons) {
-				bs.curr++;
-				focusbuttons = bs.curr < (int)bs.nbuttons ?
+				d.bs.curr++;
+				focusbuttons = d.bs.curr < (int)d.bs.nbuttons ?
 				    true : false;
 				if (focusbuttons == false) {
 					sel = 0;
-					bs.curr = conf->button.always_active ? 0 : -1;
+					d.bs.curr = conf->button.always_active ? 0 : -1;
 				}
 			} else {
 				sel++;
 				focusbuttons = sel > 2 ? true : false;
 				if (focusbuttons) {
-					bs.curr = 0;
+					d.bs.curr = 0;
 				}
 			}
-			draw_buttons(widget, bs);
-			wrefresh(widget);
+			DRAW_BUTTONS(d);
 			break;
 		case KEY_LEFT:
 			if (focusbuttons) {
-				bs.curr--;
-				focusbuttons = bs.curr < 0 ? false : true;
+				d.bs.curr--;
+				focusbuttons = d.bs.curr < 0 ? false : true;
 				if (focusbuttons == false) {
 					sel = 2;
-					bs.curr = conf->button.always_active ? 0 : -1;
+					d.bs.curr = conf->button.always_active ? 0 : -1;
 				}
 			} else {
 				sel--;
 				focusbuttons = sel < 0 ? true : false;
 				if (focusbuttons)
-					bs.curr = (int)bs.nbuttons - 1;
+					d.bs.curr = (int)d.bs.nbuttons - 1;
 			}
-			draw_buttons(widget, bs);
-			wrefresh(widget);
+			DRAW_BUTTONS(d);
 			break;
 		case KEY_UP:
 			if (focusbuttons) {
 				sel = 0;
 				focusbuttons = false;
-				bs.curr = conf->button.always_active ? 0 : -1;
-				draw_buttons(widget, bs);
-				wrefresh(widget);
-			} else { c[sel].value = c[sel].value > 0 ?
-			    c[sel].value - 1 : c[sel].max;
+				d.bs.curr = conf->button.always_active ? 0 : -1;
+				DRAW_BUTTONS(d);
+			} else {
+				c[sel].value = c[sel].value > 0 ?
+				    c[sel].value - 1 : c[sel].max;
 			}
 			break;
 		case KEY_DOWN:
@@ -189,41 +202,16 @@ bsddialog_timebox(struct bsddialog_conf *conf, const char* text, int rows,
 				break;
 			if (f1help_dialog(conf) != 0)
 				return (BSDDIALOG_ERROR);
-			/* No break, screen size can change */
+			if (timebox_redraw(&d, c) != 0)
+				return (BSDDIALOG_ERROR);
+			break;
 		case KEY_RESIZE:
-			/* Important for decreasing screen */
-			hide_dialog(y, x, h, w, conf->shadow);
-			refresh();
-
-			if (widget_size_position(conf, rows, cols, text,
-			    3 /*windows*/, MINWTIME, &bs, &y, &x, &h, &w,
-			    NULL) != 0)
+			if (timebox_redraw(&d, c) != 0)
 				return (BSDDIALOG_ERROR);
-			if (update_dialog(conf, shadow, widget, y, x, h, w,
-			    textpad, text, &bs) != 0)
-				return (BSDDIALOG_ERROR);
-
-			doupdate();
-
-			mvwaddch(widget, h - 5, w/2 - 3, ':');
-			mvwaddch(widget, h - 5, w/2 + 2, ':');
-			wrefresh(widget);
-
-			prefresh(textpad, 0, 0, y+1, x+2, y+h-7, x+w-2);
-
-			wclear(c[0].win);
-			mvwin(c[0].win, y + h - 6, x + w/2 - 7);
-			wclear(c[1].win);
-			mvwin(c[1].win, y + h - 6, x + w/2 - 2);
-			wclear(c[2].win);
-			mvwin(c[2].win, y + h - 6, x + w/2 + 3);
-
-			/* Important to avoid grey lines expanding screen */
-			refresh();
 			break;
 		default:
-			if (shortcut_buttons(input, &bs)) {
-				retval = BUTTONVALUE(bs);
+			if (shortcut_buttons(input, &d.bs)) {
+				retval = BUTTONVALUE(d.bs);
 				loop = false;
 			}
 		}
@@ -237,7 +225,7 @@ bsddialog_timebox(struct bsddialog_conf *conf, const char* text, int rows,
 
 	for (i = 0; i < 3; i++)
 		delwin(c[i].win);
-	end_dialog(conf, shadow, widget, textpad);
+	end_dialog(&d);
 
 	return (retval);
 }
