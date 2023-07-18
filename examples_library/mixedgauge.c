9af8cbb3b3a30d0fdd8abc05865e31fa21676706
Alfonso Siciliano <alfix86@gmil.com>
2023-07-18 13:50:43 +0200

    refactor mixedgauge example

--- examples_library/mixedgauge.c
+++ examples_library/mixedgauge.c
@@ -9,58 +9,69 @@
  */
 
 #include <bsddialog.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+
+#define NMINIBAR    13
+
+static const char *minilabels[NMINIBAR] = {
+	"Label   1",
+	"Label   2",
+	"Label   3",
+	"Label   4",
+	"Label   5",
+	"Label   6",
+	"Label   7",
+	"Label   8",
+	"Label   9",
+	"Label  10",
+	"Label  11",
+	"Label   X",
+	"Label   Y",
+};
+	
+static int minipercs[NMINIBAR] = {
+	BSDDIALOG_MG_SUCCEEDED,
+	BSDDIALOG_MG_FAILED,
+	BSDDIALOG_MG_PASSED,
+	BSDDIALOG_MG_COMPLETED,
+	BSDDIALOG_MG_CHECKED,
+	BSDDIALOG_MG_DONE,
+	BSDDIALOG_MG_SKIPPED,
+	BSDDIALOG_MG_INPROGRESS,
+	BSDDIALOG_MG_BLANK,
+	BSDDIALOG_MG_NA,
+	BSDDIALOG_MG_PENDING,
+	67,
+	0,
+};
+
+static void exit_error()
+{
+	if (bsddialog_inmode())
+		bsddialog_end();
+	printf("Error: %s\n", bsddialog_geterror());
+	exit (1);
+}
 
 int main()
 {
 	int retval, i;
 	struct bsddialog_conf conf;
-	const char *minilabels[12] = {
-		"Label  1",
-		"Label  2",
-		"Label  3",
-		"Label  4",
-		"Label  5",
-		"Label  6",
-		"Label  7",
-		"Label  8",
-		"Label  9",
-		"Label 10",
-		"Label 11",
-		"Label  X",
-	};
-	int minipercs[12] = {
-		BSDDIALOG_MG_SUCCEEDED,
-		BSDDIALOG_MG_FAILED,
-		BSDDIALOG_MG_PASSED,
-		BSDDIALOG_MG_COMPLETED,
-		BSDDIALOG_MG_CHECKED,
-		BSDDIALOG_MG_DONE,
-		BSDDIALOG_MG_SKIPPED,
-		BSDDIALOG_MG_INPROGRESS,
-		BSDDIALOG_MG_BLANK,
-		BSDDIALOG_MG_NA,
-		BSDDIALOG_MG_PENDING,
-		0
-	};
 
-	if (bsddialog_init() == BSDDIALOG_ERROR) {
-		printf("Error: %s\n", bsddialog_geterror());
-		return (1);
-	}
+	if (bsddialog_init() == BSDDIALOG_ERROR)
+		exit_error();
 
 	bsddialog_initconf(&conf);
 	conf.title = "mixedgauge";
 	for (i = 0; i <= 10; i++) {
-		minipercs[11] = i * 10;
+		minipercs[11] += 3;
+		minipercs[12] = i * 10;
 		retval= bsddialog_mixedgauge(&conf, "Example", 20, 40,
-		    50 + i * 5, 12, minilabels, minipercs);
-    		if(retval == BSDDIALOG_ERROR) {
-			bsddialog_end();
-			printf("Error: %s\n", bsddialog_geterror());
-			return (1);
-		}
+		    50 + i * 5, NMINIBAR, minilabels, minipercs);
+    		if(retval == BSDDIALOG_ERROR)
+			exit_error();
 		sleep(1);
 	}
 
