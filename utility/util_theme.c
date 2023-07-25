66c7c380bc8e52d62c2f0fae835f9eb0757f070c
Alfonso Siciliano <alfix86@gmil.com>
2023-07-25 16:53:03 +0200

    fix save-theme with COMPAT

--- utility/util_theme.c
+++ utility/util_theme.c
@@ -246,10 +246,7 @@ void loadtheme(const char *file, bool compatibility)
 		if (i >= NPROPERTY) {
 			if (strcmp(name, "version") == 0)
 				continue; /* nothing for now */
-			if (compatibility == false)
-				PROP_ERROR(name, "Unknown theme property name");
-			else
-				continue;
+			PROP_ERROR(name, "Unknown theme property name");
 		}
 		switch (p[i].type) {
 		case CHAR:
@@ -300,6 +297,8 @@ void loadtheme(const char *file, bool compatibility)
 			*((int*)p[i].value) = bsddialog_color(fg, bg, flags);
 			break;
 		case COMPAT:
+			if (compatibility == false)
+				PROP_ERROR(name, "Unknown theme property name");
 			/*
 			 * usr.sbin/bsdconfig/share/dialog.subr:2255
 			 * uses this parameter to set NO_SHADOW.
