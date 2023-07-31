1e7c874a64317c2e161ebc69ac2adef3aeae8dcb
Alfonso Siciliano <alfix86@gmail.com>
2023-07-31 20:40:14 +0000

    Revert "improve example with date format"
    This reverts commit e37e0f351e40272faee62934c963daf0cabe1803

--- examples_library/datebox.c
+++ examples_library/datebox.c
@@ -32,7 +32,6 @@ int main()
 	}
 	bsddialog_initconf(&conf);
 	conf.title = "datebox";
-	conf.date.format = "m/d/y"; /* d/m/y | m/d/y | y/m/d */
 	output = bsddialog_datebox(&conf, "Example", 9, 35, &yy, &mm, &dd);
 	bsddialog_end();
 	if (output == BSDDIALOG_ERROR) {
